308381f8e1e7e6f503540106f0fd35d184e92980
DanielIlinskii <95278255+DanielIlinskii@users.noreply.github.com>
2023-05-23 20:13:24 +0300

    Update enemy.py

--- game/enemy.py
+++ game/enemy.py
@@ -1,4 +1,5 @@
 from pygame import *
+from initial_setting import *
 
 
 init()
@@ -7,46 +8,69 @@ window_w = 1200
 window_h = 700
 
 window = display.set_mode((window_w, window_h))
-display.set_caption("Ghost")
 
-class Sprite:
-    def __init__(self, filename, width, height, x, y, speed):
+
+
+class Hero:
+    def __init__(self, width, height, x, y, speed):
         self.width = width
         self.height = height
-        self.image = transform.scale(image.load(filename).convert_alpha(), (self.width, self.height))
-
         self.x = x
         self.y = y
         self.speed = speed
-        self.rect = Rect(self.x, self.y, self.width, self.height)
-
-    def touch_area(self):
-        draw.rect(window, (255, 0, 0), Rect(self.x, self.y, self.width, self.height), 0)
-
-    def reset(self):
-        window.blit(self.image, (self.x, self.y))
-
-
-class Hero(Sprite):
-
-    def __init__(self, filename, width, height, x, y, speed):
-        super().__init__(filename, width, height, x, y, speed)
+        self.image = transform.scale(image.load(r'C:\Users\028\Downloads\game\sprites\walk1.png').convert_alpha(), (self.width, self.height))
         self.jump = False
         self.j_count = 10
+        self.rect = Rect(self.x, self.y, self.width, self.height)
+
+    
+    def touch_area(self):
+        draw.rect(window, (255, 0, 0), Rect(self.x, self.y, self.width, self.height), 1)
+    
+    def draw(self):
+        window.blit(self.image, (self.x, self.y))
+        self.rect.x = self.x
+        self.rect.y = self.y
+    
+    def logic(self):
+        for enemy in small_enemy_list:
+            if enemy.rect.colliderect(self.rect):
+                small_enemy_list.remove(enemy)
+
+
+
+
+
+
+class Enemy:
+    def __init__(self, width, height, x, y, speed):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.image = transform.scale(image.load(r'C:\Users\028\Downloads\game\sprites\enemy.png').convert_alpha(), (self.width, self.height))
+        self.rect = Rect(self.x, self.y, self.width, self.height)
+        small_enemy_list.append(self)
     
     def update(self):
-        pass
+        self.x -= self.speed
+        self.rect.x = self.x
+        if self.x < 0:
+            small_enemy_list.remove(self)
+        
+    
+    def touch_area(self):
+        draw.rect(window, (255, 0, 0), Rect(self.x, self.y, self.width, self.height), 1)
+    
+    def draw(self):
+        window.blit(self.image, (self.x, self.y))
+    
+    def logic(self):
+        self.update()
+        self.draw()
+        self.touch_area()
 
 
-
-bg = Sprite(r'C:\Users\028\Downloads\game\sprites\background.jpg', window_w, window_h, 0, 0, 1)
-bg1 = Sprite(r'C:\Users\028\Downloads\game\sprites\background.jpg', window_w, window_h, bg.width, 0, 1)
-
-
-small_enemy = Sprite(r"C:\Users\028\Downloads\game\sprites\enemy.png", 48, 48, 820, 160, 2) # small enemy
-
-hero = Hero(r'C:\Users\028\Downloads\game\sprites\walk1.png', 48, 48, 15, 200, 10) # main person
-
-
-
-exit()
+hero = Hero(48, 48, 15, 200, 10)
+small_enemy_list = []
